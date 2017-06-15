298115d43fed9438e2a4f0c1f5a0d5ec3188a345
礼蓓 <libei.ly@alibaba-inc.com>
2017-06-15 16:14:07 +0800

    canvas test

--- weapp/pages/index/index.js
+++ weapp/pages/index/index.js
@@ -4,14 +4,36 @@ var r = require('./require.js');//引入js的方式
 Page({
   data: {
     globalData:app.globalData.testData,
-    test:'123'
+    test:'123',
+    cutted_url:'./test3.jpeg'
   },
   buttonClik:function () {
-    r.test.bind(this)();
-    console.log(r.test1());
-    console.log(app.commonTest());
-    this.setData({
-      globalData: app.commonTest()
+    // r.test.bind(this)();
+    // console.log(r.test1());
+    // console.log(app.commonTest());
+    // this.setData({
+    //   globalData: app.commonTest()
+    // })
+    var that = this;
+    const ctx = wx.createCanvasContext('myCanvas');
+    wx.chooseImage({
+      success: function(res){
+        ctx.drawImage(res.tempFilePaths[0], 0, 0, 300, 300)
+        ctx.draw();
+        setTimeout(function () {
+          wx.canvasToTempFilePath({
+            x: 0,
+            y: 0,
+            canvasId: 'myCanvas',
+            success: function (res) {
+              that.setData({
+                cutted_url: res.tempFilePath
+              })
+            }
+          },2000)
+        })
+
+      }
     })
   }
 })
