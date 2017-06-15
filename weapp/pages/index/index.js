bee031a4941ccee21536ea6db27f449ecd513c1c
礼蓓 <libei.ly@alibaba-inc.com>
2017-06-15 16:26:10 +0800

    delete setTimeout

--- weapp/pages/index/index.js
+++ weapp/pages/index/index.js
@@ -1,11 +1,10 @@
-
 var app = getApp();
 var r = require('./require.js');//引入js的方式
 Page({
   data: {
     globalData:app.globalData.testData,
     test:'123',
-    cutted_url:'./test3.jpeg'
+    cutted_url:''
   },
   buttonClik:function () {
     // r.test.bind(this)();
@@ -20,20 +19,22 @@ Page({
       success: function(res){
         ctx.drawImage(res.tempFilePaths[0], 0, 0, 300, 300)
         ctx.draw();
-        setTimeout(function () {
-          wx.canvasToTempFilePath({
-            x: 0,
-            y: 0,
-            canvasId: 'myCanvas',
-            success: function (res) {
-              that.setData({
-                cutted_url: res.tempFilePath
-              })
-            }
-          },2000)
-        })
-
+        that.cut()
       }
     })
-  }
+  },
+
+  cut: function() {
+    var that = this
+    wx.canvasToTempFilePath({
+      x: 0,
+      y: 0,
+      canvasId: 'myCanvas',
+      success: function (res) {
+        that.setData({
+          cutted_url: res.tempFilePath
+        })
+      }
+    })
+  },
 })
