356a573dab49f2be66e89bed802bf9c28744ce59
礼蓓 <libei.ly@alibaba-inc.com>
2017-06-20 14:38:51 +0800

    test html2wxml

--- weapp/pages/index/index.js
+++ weapp/pages/index/index.js
@@ -1,10 +1,13 @@
 var app = getApp();
 var r = require('./require.js');//引入js的方式
+import {html2json} from '../../utils/lib/html2wxml/html2wxml';
 Page({
   data: {
     globalData:app.globalData.testData,
     test:'123',
-    cutted_url:''
+    cutted_url:'',
+    img:'',
+    innerHTML: html2json('<div><p id ="1_1">test</p>').child
   },
   buttonClik:function () {
     // r.test.bind(this)();
@@ -19,12 +22,18 @@ Page({
       success: function(res){
         ctx.drawImage(res.tempFilePaths[0], 0, 0, 300, 300)
         ctx.draw();
+        // timeout(1000).then(function () {
+        //   // that.cut()
+        // });
         that.cut()
       }
-    })
+    });
+
   },
 
+
   cut: function() {
+    console.log('cut')
     var that = this
     wx.canvasToTempFilePath({
       x: 0,
@@ -34,7 +43,16 @@ Page({
         that.setData({
           cutted_url: res.tempFilePath
         })
+      },
+      fail: function(){
+        console.log('fail')
       }
     })
   },
 })
+
+function timeout(ms) {
+  return new Promise(function(resolve)  {
+        setTimeout(resolve, ms);
+})
+}
